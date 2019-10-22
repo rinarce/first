d1171ab2310a318d2c734f553c05f82a6478dd29
Evgheni Celnocov <eugennix@gmail.com>
2019-10-22 14:47:44 +0300

    2019-10-22

---
+++ hi_there.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void){
+printf("Hello, dad!");
+
+// dad answer
+print("\n- Hello, daughter!\n");
+print("\n- How are you?\n");
+print("\n- Good luck!\n");
+
+return 0;}
