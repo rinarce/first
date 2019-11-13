558f43d30caab6d02fcd7b4393f76b2892baefd0
Evgheni Celnocov <eugennix@gmail.com>
2019-11-13 03:38:57 +0300

    2019-11-13 Ograblenie OK

--- Scroot/A402Ograblenie/A402ograblenie/A402ograblenie.c
+++ Scroot/A402Ograblenie/A402ograblenie/A402ograblenie.c
@@ -1,43 +1,20 @@
-﻿// A402ograblenie.c
-
-#include <stdio.h>
-
+﻿#include <stdio.h>              // A402ograblenie.c
 #pragma warning(disable:4996)   // for use scanf in VisualStudio
 
-#define MAX_LINE_LEN    1000    // 1 кБайт на строку 
-#define MAX_LINES_COUNT 1000    // 1000 строк * 2 (исход + кодиров) = 2Мбайт
-#define KEY_LEN         100     // Длина ключа
+#define MAX_LINE_LEN    520      
+#define MAX_LINES_COUNT 21       
+#define DEBUG
 
 
 typedef struct codedLine {
-  int len;
   char origin [MAX_LINE_LEN];   // исходная строка со *
   char coded  [MAX_LINE_LEN];   // закодированная (переведена из шестнадцателричного вида)
 } codedLine;
 
 
-// длина строки до '\0'
-unsigned int _strLenght(char const* str) {
-  int lenght = 0;
-  if (NULL != str)
-    while (*str++)
-      lenght++;
-  return lenght;
-}
-
-
-
-// return 1 = лат буквы + пробел
+// return 1, если лат буква или пробел
 int _charIsLetterOrSpace(char c) {
   if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')
-    return 1;
-  return 0;
-}
-
-// return 1 = знаки препинания ".,!?;:-()"  0 = всё остальное
-int _charIsSeparator(char c) {
-  if (c == '.' || c == ',' || c == '!' || c == '?' || c == ';'
-               || c == ':' || c == '-' || c == '(' || c == ')')
     return 1;
   return 0;
 }
@@ -46,89 +23,106 @@ int _charIsSeparator(char c) {
 // получает указатель на строку - 2 первых символа шестначатеричные символы
 // возврат - char байт из этих символов
 char _convert2HexInChar(char const* hex) {
-  int digit1 = -1, 
-      digit2 = -1;
-  char t;
-  /* // без проверок
-  if (NULL == hex) 
-    return 0;       // не передан нормальный указатель
-  if ('\0' == *hex || '\0' == *(hex + 1))
-    return 0;       // в строке нет 2х символов, найден конец строки
-  */
+  char digit1, digit2, t;
+  t = *hex;                                                 // 1й символ
+  if      ('0' <= t && t <= '9')   digit1 = t - '0';
+  else if ('A' <= t && t <= 'F')   digit1 = t - 'A' + 10;
+  else if ('a' <= t && t <= 'f')   digit1 = t - 'a' + 10;
+  t = *(hex + 1);                                           // 2й символ
+  if      ('0' <= t && t <= '9')   digit2 = t - '0';
+  else if ('A' <= t && t <= 'F')   digit2 = t - 'A' + 10;
+  else if ('a' <= t && t <= 'f')   digit2 = t - 'a' + 10;
 
-  // 1й символ
-  t = *hex;
-  if      ('0' <= t && t <= '9')   digit1 = (int)(t - '0');
-  else if ('A' <= t && t <= 'F')   digit1 = (int)(t - 'A' + 10);
-  else if ('a' <= t && t <= 'f')   digit1 = (int)(t - 'a' + 10);
-
-  // 2й символ
-  t = *(hex + 1);
-  if      ('0' <= t && t <= '9')   digit2 = (int)(t - '0');
-  else if ('A' <= t && t <= 'F')   digit2 = (int)(t - 'A' + 10);
-  else if ('a' <= t && t <= 'f')   digit2 = (int)(t - 'a' + 10);
-
-  //if (-1 == digit1 || -1 == digit2)
-  //  return 0; // неподходящие символы - не удалось перекодировать
-
-  return (char)(digit1*16 + digit2); // OK
+  return (char)(digit1*16 + digit2); 
 }
-
 
 
 int main()
 {
   static codedLine text[MAX_LINES_COUNT] = { 0 };   // тут хранятся строки
   static char key[MAX_LINE_LEN] = { 0 };            // ключ
-  static char buffer[MAX_LINE_LEN * 2] = { 0 };
+  static char buffer[MAX_LINE_LEN * 2] = { 0 };     // буфер для 16ричных кодированных строк
   int nLines = 0;                                   // число строк
-  int lineIndex, buffIndex, symIndex, len, keyIndex;
-  int keyLen = KEY_LEN;
+  int messageLen = 0;                               // длина одного сообщения
+  
+  int lineIndex, buffIndex, symIndex, keyIndex, index, goodKey;
+  char x;
 
-  scanf("%d", &nLines);
-
-  // ввод оригиналов строк с затёртыми смиволами
-  for (lineIndex = 0; lineIndex < nLines; ++lineIndex) {
-    scanf("%s", &text[lineIndex].origin);
-    // запомню длину строк
-    text[lineIndex].len = _strLenght(text[lineIndex].origin);
-  }
-
+  scanf("%d%d", &nLines, &messageLen);
+  
   // ввод строк с кодом
   for (lineIndex = 0; lineIndex < nLines; ++lineIndex) {
     scanf("%s", &buffer);
     // каждые 2 символа кода 16ричного вида -> 1 символ байт
-    for (buffIndex = 0; buffer[buffIndex] != '\0'; ++buffIndex)
+    for (buffIndex = 0; buffer[buffIndex] != '\0'; buffIndex += 2)
       text[lineIndex].coded[buffIndex / 2] = _convert2HexInChar(&buffer[buffIndex]);
-
-    text[lineIndex].coded[buffIndex / 2] = '\0'; // конец строки
   }
 
+  // ввод оригиналов строк с затёртыми смиволами
+  for (lineIndex = 0; lineIndex < nLines; ++lineIndex)
+    scanf("%s", &text[lineIndex].origin);
+
+
+  // РАСШИФРОВКА
+  printf("\nStep 1\n");
   // вычисление ключа в позициях где известны исходные символы
   for (lineIndex = 0; lineIndex < nLines; ++lineIndex) {
-    len = text[lineIndex].len;
-    for (symIndex = 0; symIndex < len; ++symIndex) {
-      if (text[lineIndex].origin[symIndex] != '*')
+    printf("\n%.2d >> %s\n      ", lineIndex, text[lineIndex].origin);   
+    for (symIndex = 0; symIndex < messageLen; ++symIndex) {
+      if (text[lineIndex].origin[symIndex] != '*') // не звёздочка, можно вычислить ключ
         key[symIndex] = text[lineIndex].origin[symIndex] ^ text[lineIndex].coded[symIndex];
+      printf("%c", key[symIndex]);
     }
   }
 
-  // найти ключ в остальных позициях
-  for (keyIndex = 0; keyIndex < keyLen; ++keyIndex) {
-    if (key[keyIndex] == 0) { // ключ в этой позиции не определён ранее
-      // Как-то найти ключ, известно:
-      // 1. В этой позиции в какой-то строке исходного текста обязательно будет пробел
-      // 2. Ключ и исходный текст в этой позиции - только допустимые символы
-      // 3. Предполагая пробел в каждой строке по очереди, определить вариант ключа
-      // 4. Посмотреть, даёт ли этот вариант со всеми другими строками допустимые символы
-      // 5. Если да - нашли ключ, нет - предполагаем что пробел в другой строке
+  printf("\n\nStep 2\n");
+  // Как-то найти ключ в остальных поициях, известно:
+  // 1. В этой позиции в какой-то строке исходного текста обязательно будет пробел
+  // 2. Исходный текст в этой позиции - только допустимые символы
+  // 3. Предполагая пробел в каждой строке по очереди, определяю вариант ключа
+  // 4. Посмотрю, даёт ли этот вариант со всеми другими строками допустимые символы
+  // 5. Если да - ключ найден, нет - предполагаю что пробел в другой строке
 
+  for (keyIndex = 0; keyIndex < messageLen; ++keyIndex) {
+    if (key[keyIndex] == 0) {             // ключ в этой позиции не определён ранее
+      printf("\nFind key[%2d]", keyIndex);
+      // предположим пробел в какой-то строке lineIndex
+      for (lineIndex = 0; lineIndex < nLines; ++lineIndex) {
+        // если пробел в этой строке, то ключ будет вот такой
+        x = ' ' ^ text[lineIndex].coded[keyIndex]; 
+        printf("\n<line [%d] give key[%c]>", lineIndex, x);
+
+        // проверим все остальные строки ;-) используя этот ключ
+        goodKey = 1;
+        for (index = 0; index < nLines; ++index) {
+          printf("[%c]", text[index].coded[keyIndex] ^ x); 
+          if (!_charIsLetterOrSpace(text[index].coded[keyIndex] ^ x)) {
+            goodKey = 0;  // если хоть в одной строке даст неправильную расшифровку
+            printf(" xxx bad!");
+            break;        // дальше не проверять
+          }
+        }
+        if (goodKey) {    // во всех строках получилось что-то осмысленное => ключ найден
+          key[keyIndex] = x;
+          printf("\nKEY [%2d] FOUND = [%c]\n", keyIndex, x);
+          break;          // для ключа в позиции keyIndex - остальные строки не проверять
+        }
+      }
     }
   }
 
+  printf("\n\n---KEY---\n[%s]\n\n ---RESULT--\n\n", key);
 
-  // Напечатать ответ
+  // Ключ в 16ричном виде
+  for (keyIndex = 0; keyIndex < messageLen; ++keyIndex)
+    printf("%X", key[keyIndex]);
+  printf("\n");       // конец ключа 
 
+  // Расшифровать сообщение
+  for (lineIndex = 0; lineIndex < nLines; ++lineIndex) {
+    for (symIndex = 0; symIndex < messageLen; ++symIndex) 
+      printf("%c", text[lineIndex].coded[symIndex] ^ key[symIndex]);
+    printf("\n");     // конец каждой строки 
+  }
   return 0;
 }
-
